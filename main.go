d8e043e10d7dd2cd63d0797c9f4759856818dd66
gsxhnd <gsxhnd@gmail.com>
2022-02-10 18:34:59 +0800

    refactor: rm cmd dir

--- main.go
+++ main.go
@@ -1,13 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime"
 
-	"github.com/heart-dance/seed/src/cmd"
+	"github.com/urfave/cli/v2"
 )
 
+var RootCmd = cli.NewApp()
+
+var (
+	version  = "0.0.1"
+	hostFlag = &cli.StringFlag{
+		Name:     "host",
+		Required: true,
+		Usage:    "listen host",
+	}
+	webFlag = &cli.StringFlag{
+		Name:  "web",
+		Usage: "web assets path",
+	}
+	profileFlag = &cli.StringFlag{
+		Name:     "profile",
+		Required: true,
+		Usage:    "profile path",
+	}
+)
+
+func init() {
+	RootCmd.Copyright = "Copyright © 2020 gsxhnd"
+	RootCmd.Usage = "A Download Tool"
+	RootCmd.Commands = nil
+	RootCmd.HideHelpCommand = true
+	RootCmd.Version = version + " " + runtime.GOARCH + "/" + runtime.GOOS
+	RootCmd.Flags = []cli.Flag{
+		hostFlag,
+		profileFlag,
+		webFlag,
+	}
+	RootCmd.Before = func(c *cli.Context) error {
+		return nil
+	}
+	RootCmd.Action = func(c *cli.Context) error {
+		fmt.Println(c.String("host"))
+		fmt.Println(c.String("profile"))
+		fmt.Println(c.String("web"))
+		// var app = src.NewApplication()
+		// app.Run()
+		return nil
+	}
+}
+
 func main() {
-	err := cmd.RootCmd.Run(os.Args)
+	err := RootCmd.Run(os.Args)
 	if err != nil {
 		panic(err)
 	}
---
+++ src/utils/profile.go
@@ -0,0 +1,9 @@
+package utils
+
+import "os"
+
+func CheckProfile(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.MkdirAll(path, os.ModePerm)
+	}
+}
